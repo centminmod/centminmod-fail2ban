e5e14702c8f10752798e3c76c0aa0c2b67e9ba92
George Liu <eva2000@centminmod.com>
2017-05-13 02:52:58 +1000

    update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -56,6 +56,9 @@ status() {
     fail2ban-client status $j
     done
     echo "---------------------------------------"
+    echo "Banned IP Stats:"
+    awk '/Ban/ {print $NF}' /var/log/fail2ban.log | sort | uniq -c | sort -n
+    echo "---------------------------------------"
 }
 
 pipinstall() {
