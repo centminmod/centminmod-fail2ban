afd80cc2fc799b007d74a35117a6949f1ad76bb4
George Liu <eva2000@centminmod.com>
2017-05-18 07:57:00 +1000

    update fail2ban.sh status routine
    - fix fail2ban ban ip stats

--- fail2ban.sh
+++ fail2ban.sh
@@ -92,28 +92,28 @@ status() {
     done
     echo "---------------------------------------"
     echo "All Time: Top 10 Banned IP Addresses:"
-    zgrep -h "] Ban " /var/log/fail2ban.log* | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
+    zgrep -h "] Ban " /var/log/fail2ban.log{-*,*} | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
     echo "---------------------------------------"
     echo "All Time: Top 10 Restored Banned IP Addresses:"
-    zgrep -h "Restore Ban " /var/log/fail2ban.log* | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
+    zgrep -h "Restore Ban " /var/log/fail2ban.log{-*,*} | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
     echo "---------------------------------------"
     echo "Yesterday: Top 10 Banned IP Addresses:"
-    zgrep -h "] Ban " /var/log/fail2ban.log* | grep `date -d "1 day ago" +%Y-%m-%d` | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
+    zgrep -h "] Ban " /var/log/fail2ban.log{-*,*} | grep `date -d "1 day ago" +%Y-%m-%d` | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
     echo "---------------------------------------"
     echo "Yesterday: Top 10 Restored Banned IP Addresses:"
-    zgrep -h "Restore Ban " /var/log/fail2ban.log* | grep `date -d "1 day ago" +%Y-%m-%d` | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
+    zgrep -h "Restore Ban " /var/log/fail2ban.log{-*,*} | grep `date -d "1 day ago" +%Y-%m-%d` | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
     echo "---------------------------------------"
     echo "Today: Top 10 Banned IP Addresses:"
-    zgrep -h "] Ban " /var/log/fail2ban.log* | grep `date +%Y-%m-%d` | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
+    zgrep -h "] Ban " /var/log/fail2ban.log{-*,*} | grep `date +%Y-%m-%d` | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
     echo "---------------------------------------"
     echo "Today: Top 10 Restored Banned IP Addresses:"
-    zgrep -h "Restore Ban " /var/log/fail2ban.log* | grep `date +%Y-%m-%d` | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
+    zgrep -h "Restore Ban " /var/log/fail2ban.log{-*,*} | grep `date +%Y-%m-%d` | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
     echo "---------------------------------------"
     echo "1 hr ago: Top 10 Banned IP Addresses:"
-    zgrep -h "] Ban " /var/log/fail2ban.log* | grep "$(date -d "1 hour ago" '+%Y-%m-%d %H:')" | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
+    zgrep -h "] Ban " /var/log/fail2ban.log{-*,*} | grep "$(date -d "1 hour ago" '+%Y-%m-%d %H:')" | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
     echo "---------------------------------------"
     echo "1 hr ago: Top 10 Restored Banned IP Addresses:"
-    zgrep -h "Restore Ban " /var/log/fail2ban.log* | grep "$(date -d "1 hour ago" '+%Y-%m-%d %H:')" | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
+    zgrep -h "Restore Ban " /var/log/fail2ban.log{-*,*} | grep "$(date -d "1 hour ago" '+%Y-%m-%d %H:')" | awk '{print $NF, $6}' | sort | uniq -c | sort -rn | tail -10
     echo "---------------------------------------"
 }
 
