91f111c667e83e10218a069b2cf699b779e28fa3
George Liu (eva2000) <eva2000@centminmod.com>
2023-04-07 05:40:22 +1000

    initial prep for fail2ban 1.0.x support on both el7 and el8

--- fail2ban.sh
+++ fail2ban.sh
@@ -5,12 +5,10 @@
 ######################################################
 # variables
 #############
-VER=0.12
+VER=0.13
 DT=$(date +"%d%m%y-%H%M%S")
-# branch version https://github.com/fail2ban/fail2ban/branches/active
-# 0.11.2 downgrade to 0.10.x requires changes to sqllit3 database outlined
-# at # https://github.com/fail2ban/fail2ban/blob/0.11.2/ChangeLog
-FAILBAN_VER='0.11'
+# https://github.com/fail2ban/fail2ban/tags
+FAIL2BAN_TAG="1.0.2"
 
 USERIP=$(last -i | grep "still logged in" | awk '{print $3}' | uniq)
 SERVERIPS=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
@@ -21,10 +19,16 @@ DIR_TMP='/svr-setup'
 #############
 CENTOSVER=$(awk '{ print $3 }' /etc/redhat-release)
 
+KERNEL_NUMERICVER=$(uname -r | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }')
+
 if [ "$CENTOSVER" == 'release' ]; then
     CENTOSVER=$(awk '{ print $4 }' /etc/redhat-release | cut -d . -f1,2)
     if [[ "$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1)" = '7' ]]; then
         CENTOS_SEVEN='7'
+    elif [[ "$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1)" = '8' ]]; then
+        CENTOS_EIGHT='8'
+    elif [[ "$(cat /etc/redhat-release | awk '{ print $4 }' | cut -d . -f1)" = '9' ]]; then
+        CENTOS_NINE='9'
     fi
 fi
 
@@ -32,17 +36,89 @@ if [[ "$(cat /etc/redhat-release | awk '{ print $3 }' | cut -d . -f1)" = '6' ]];
     CENTOS_SIX='6'
 fi
 
+# Check for Redhat Enterprise Linux 7.x
 if [ "$CENTOSVER" == 'Enterprise' ]; then
-    CENTOSVER=$(cat /etc/redhat-release | awk '{ print $7 }')
-    OLS='y'
+    CENTOSVER=$(awk '{ print $7 }' /etc/redhat-release)
+    if [[ "$(awk '{ print $1,$2 }' /etc/redhat-release)" = 'Red Hat' && "$(awk '{ print $7 }' /etc/redhat-release | cut -d . -f1)" = '7' ]]; then
+        CENTOS_SEVEN='7'
+        REDHAT_SEVEN='y'
+    fi
 fi
 
 if [[ -f /etc/system-release && "$(awk '{print $1,$2,$3}' /etc/system-release)" = 'Amazon Linux AMI' ]]; then
     CENTOS_SIX='6'
 fi
 
-if [[ "$CENTOS_SEVEN" != '7' ]]; then
-  echo "CentOS 7.x Only"
+# ensure only el8+ OS versions are being looked at for alma linux, rocky linux
+# oracle linux, vzlinux, circle linux, navy linux, euro linux
+EL_VERID=$(awk -F '=' '/VERSION_ID/ {print $2}' /etc/os-release | sed -e 's|"||g' | cut -d . -f1)
+if [ -f /etc/almalinux-release ] && [[ "$EL_VERID" -eq 8 || "$EL_VERID" -eq 9 ]]; then
+  CENTOSVER=$(awk '{ print $3 }' /etc/almalinux-release | cut -d . -f1,2)
+  if [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '8' ]]; then
+    CENTOS_EIGHT='8'
+    ALMALINUX_EIGHT='8'
+  elif [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '9' ]]; then
+    CENTOS_NINE='9'
+    ALMALINUX_NINE='9'
+  fi
+elif [ -f /etc/rocky-release ] && [[ "$EL_VERID" -eq 8 || "$EL_VERID" -eq 9 ]]; then
+  CENTOSVER=$(awk '{ print $4 }' /etc/rocky-release | cut -d . -f1,2)
+  if [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '8' ]]; then
+    CENTOS_EIGHT='8'
+    ROCKYLINUX_EIGHT='8'
+  elif [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '9' ]]; then
+    CENTOS_NINE='9'
+    ROCKYLINUX_NINE='9'
+  fi
+elif [ -f /etc/oracle-release ] && [[ "$EL_VERID" -eq 8 || "$EL_VERID" -eq 9 ]]; then
+  CENTOSVER=$(awk '{ print $5 }' /etc/oracle-release | cut -d . -f1,2)
+  if [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '8' ]]; then
+    CENTOS_EIGHT='8'
+    ORACLELINUX_EIGHT='8'
+  elif [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '9' ]]; then
+    CENTOS_NINE='9'
+    ORACLELINUX_NINE='9'
+  fi
+elif [ -f /etc/vzlinux-release ] && [[ "$EL_VERID" -eq 8 || "$EL_VERID" -eq 9 ]]; then
+  CENTOSVER=$(awk '{ print $4 }' /etc/vzlinux-release | cut -d . -f1,2)
+  if [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '8' ]]; then
+    CENTOS_EIGHT='8'
+    VZLINUX_EIGHT='8'
+  elif [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '9' ]]; then
+    CENTOS_NINE='9'
+    VZLINUX_NINE='9'
+  fi
+elif [ -f /etc/circle-release ] && [[ "$EL_VERID" -eq 8 || "$EL_VERID" -eq 9 ]]; then
+  CENTOSVER=$(awk '{ print $4 }' /etc/circle-release | cut -d . -f1,2)
+  if [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '8' ]]; then
+    CENTOS_EIGHT='8'
+    CIRCLELINUX_EIGHT='8'
+  elif [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '9' ]]; then
+    CENTOS_NINE='9'
+    CIRCLELINUX_NINE='9'
+  fi
+elif [ -f /etc/navylinux-release ] && [[ "$EL_VERID" -eq 8 || "$EL_VERID" -eq 9 ]]; then
+  CENTOSVER=$(awk '{ print $5 }' /etc/navylinux-release | cut -d . -f1,2)
+  if [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '8' ]]; then
+    CENTOS_EIGHT='8'
+    NAVYLINUX_EIGHT='8'
+  elif [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '9' ]]; then
+    CENTOS_NINE='9'
+    NAVYLINUX_NINE='9'
+  fi
+elif [ -f /etc/el-release ] && [[ "$EL_VERID" -eq 8 || "$EL_VERID" -eq 9 ]]; then
+  CENTOSVER=$(awk '{ print $3 }' /etc/el-release | cut -d . -f1,2)
+  if [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '8' ]]; then
+    CENTOS_EIGHT='8'
+    EUROLINUX_EIGHT='8'
+  elif [[ "$(echo $CENTOSVER | cut -d . -f1)" -eq '9' ]]; then
+    CENTOS_NINE='9'
+    EUROLINUX_NINE='9'
+  fi
+fi
+
+if [[ -z "$CENTOS_SEVEN" && -z "$CENTOS_EIGHT" ]] || [[ "$CENTOS_SEVEN" != '7' && "$CENTOS_EIGHT" != '8' ]]; then
+  echo "CentOS 7.x and 8.x Only"
   exit
 fi
 
@@ -151,12 +227,27 @@ status() {
     fi
     echo "---------------------------------------"
 }
-
 pipinstall() {
-if [ ! -f /usr/bin/pip ]; then
-  yum -q -y install python2-pip
-  PYTHONWARNINGS=ignore:::pip._internal.cli.base_command pip install --upgrade pip
-fi
+  if [ "$CENTOS_SEVEN" == '7' ]; then
+    if [ ! -f /usr/bin/pip3 ]; then
+      yum -q -y install python3 python3-pip
+      pip3 install --upgrade pip
+    fi
+    if ! rpm -q python3-setuptools >/dev/null 2>&1; then
+      yum -q -y install python3-setuptools
+    fi
+  elif [ "$CENTOS_EIGHT" == '8' ]; then
+    if [ ! -f /usr/bin/pip3 ]; then
+      yum -q -y install python36 python3-pip
+      pip3 install --upgrade pip
+    fi
+    if ! rpm -q python3-setuptools >/dev/null 2>&1; then
+      yum -q -y install python3-setuptools
+    fi
+  else
+    echo "Only CentOS 7.x and 8.x are supported."
+    exit
+  fi
 }
 
 install() {
@@ -166,12 +257,13 @@ install() {
     echo
     cd "$DIR_TMP"
     pipinstall
-    PYTHONWARNINGS=ignore:::pip._internal.cli.base_command pip install pyinotify
-    git clone -b ${FAILBAN_VER} https://github.com/fail2ban/fail2ban
+    pip3 install pyinotify
+    git clone https://github.com/fail2ban/fail2ban
     cd fail2ban
-    git pull
-    PYTHONWARNINGS=ignore:::pip._internal.cli.base_command python setup.py install
-    if [[ "$CENTOS_SEVEN" = '7' ]]; then
+    git fetch --tags
+    git checkout ${FAIL2BAN_TAG}
+    python3 setup.py install
+    if [[ "$CENTOS_SEVEN" = '7' || "$CENTOS_EIGHT" '8' ]]; then
         \cp -f build/fail2ban.service /usr/lib/systemd/system/fail2ban.service
         \cp -f files/fail2ban-tmpfiles.conf /usr/lib/tmpfiles.d/fail2ban.conf
         \cp -f files/fail2ban-logrotate /etc/logrotate.d/fail2ban
@@ -303,7 +395,7 @@ install() {
         touch /var/log/fail2ban.log
     fi
 
-    if [[ "$CENTOS_SEVEN" = '7' ]]; then
+    if [[ "$CENTOS_SEVEN" = '7' || "$CENTOS_EIGHT" '8' ]]; then
         systemctl daemon-reload
         systemctl stop fail2ban
         systemctl start fail2ban
